260387ec31198e4e73cae0ad58be4d93477b3ce2
kathi <crq229@alumni.ku.dk>
2022-05-20 13:46:20 +0200

    updated the find_start_end_index() function so that it uses the "inner bounds" of the interval and does not possibly create any problems when interpolating (more save solution)

--- icecore_data/src/read_d18O.py
+++ icecore_data/src/read_d18O.py
@@ -12,7 +12,7 @@ def read_data_d18O(path_data):
 
 def find_start_end_index(ice_age_data, start_year, end_year):
     t_start_ind = np.min(np.where(ice_age_data >= start_year))
-    t_end_ind = np.min(np.where(ice_age_data >= end_year))
+    t_end_ind = np.max(np.where(ice_age_data <= end_year))
     return t_start_ind, t_end_ind
 
 
@@ -26,7 +26,7 @@ def get_interval_data(depth_data, d18O_data, ice_age_data, start_year, end_year,
     return depth_data_interval, d18O_interval, ice_age_interval, d18O_smooth, time_grid
 
 
-def get_interval_data_noTimeGrid(depth_data, d18O_data, ice_age_data, start_year, end_year, cop):
+def get_interval_data_noTimeGrid(depth_data, d18O_data, ice_age_data, start_year, end_year):
     t_start_ind, t_end_ind = find_start_end_index(ice_age_data, start_year, end_year)
     depth_data_interval = depth_data[t_start_ind: t_end_ind]
     d18O_interval = d18O_data[t_start_ind: t_end_ind]
